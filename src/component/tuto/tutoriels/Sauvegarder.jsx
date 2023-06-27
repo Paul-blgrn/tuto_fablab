bf75dce112da023f18f26feeda908124f4526e3b
unknown <paul.blgrn@gmail.com>
2023-06-27 18:39:19 +0200

    css update

--- src/component/tuto/tutoriels/Sauvegarder.jsx
+++ src/component/tuto/tutoriels/Sauvegarder.jsx
@@ -9,11 +9,88 @@ const Sauvegarder = () => {
                 <h2 className='Home-Title2'>Exemple:</h2>
             </div>
             <div className='imagestuto'>
-                <img className='keyboard' src='/images/keyboard.svg' alt='ctrl keyboard'/>
-                <div className="pushtouch">
-                    <div className="pushtouch-ctrl pushtouch-animate"></div>
-                    <div className="pushtouch-s pushtouch-animate"></div>
-                    <div className="pushtouch-test"></div>
+                <div className="Keyboard-parent">
+                    <img className="keyboard" src='/images/keyboard.svg' alt='ctrl keyboard'/>
+                    <div className="push-keys">
+                        {/* LINE 3 */}
+                        <div className="keyboard-line3">
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                            <div className="touch"></div>
+                        </div>
+
+                        {/* LINE 4 */}
+                        <div className="keyboard-line4">
+                            <div className="touch2"></div>
+                            <div className="touch2 key-animate"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                            <div className="touch2"></div>
+                        </div>
+
+                        {/* LINE 5 */}
+                        <div className="keyboard-line5">
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                            <div className="touch3"></div>
+                        </div>
+
+                        {/* LINE 6 */}
+                        <div className="keyboard-line6">
+                            <div className="touch6 key-animate"></div>
+                            <div className="touch6"></div>
+                            <div className="touch6"></div>
+                            <div className="touch6"></div>
+                            <div className="touch6"></div>
+                            <div className="touch6"></div>
+                            <div className="touch6"></div>
+                        </div>
+
+                        {/* NUM PAD */}
+                        <div className="keyboard-pad">
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                            <div className="touchpad"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
